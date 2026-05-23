648cb1ac63aa3cabc48fa613f31dc070be75d7ed
林展丞 <jjwer210184@gmail.com>
2026-05-23 17:57:39 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/stockfeel-cdp-csv-cleaner/',
+  base: '/sf_group_cdp_cleaner/',
   plugins: [react(), tailwindcss()],
 })
